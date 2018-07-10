f974f12d95af40c2d22c9fd683eb5b4f09e2e284
Arttu Tamminen <arttu.tamminen@kapsi.fi>
2018-07-10 23:04:00 +0300

    initial main.cpp

---
+++ main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main(int argc, char* args [])
+{
+    std::cout << "shader system version 0.1" << std::endl << "by tähtituho 2018" << std::endl;
+    std::cin.get();
+    return 0;
+}
