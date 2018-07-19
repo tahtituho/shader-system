fb66c8940773c4bce2d9538650e3f483f655949b
helgrima <arttu.tamminen@kapsi.fi>
2018-07-19 22:15:02 +0300

    changed screen dimensions to GLint to ensure max compatibility

--- main.cpp
+++ main.cpp
@@ -8,9 +8,9 @@
 #include <GL/glew.h>
 #include <GL/freeglut.h>
 
-const int SCREEN_WIDTH = 640;
-const int SCREEN_HEIGHT = 480;
-const int SCREEN_FPS = 60;
+const GLint SCREEN_WIDTH = 640;
+const GLint SCREEN_HEIGHT = 480;
+const GLint SCREEN_FPS = 60;
 
 bool initGL();
 void update();
